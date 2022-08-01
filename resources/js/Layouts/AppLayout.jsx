e79f2c6ac426a0a444b72a6b442787bc960d7299
Nikola Saratlija <saratlijanikola@gmail.com>
2022-08-01 21:59:47 +0200

    extended base layout

--- resources/js/Layouts/AppLayout.jsx
+++ resources/js/Layouts/AppLayout.jsx
@@ -13,11 +13,9 @@ export default function AppLayout({auth, children}) {
                 <div className={"flex items-center"}>
                     <div className="px-6 py-4 sm:block">
                         {auth.user ? (
-
                             <Link href={route('logout')} className="text-sm text-gray-700 underline">
                                 Log out
                             </Link>
-
                         ) : (
                             <>
                                 <Link href={route('login')} className="text-sm text-gray-700 underline">
@@ -38,7 +36,40 @@ export default function AppLayout({auth, children}) {
             </header>
 
             <div className="bg-gray-background py-10">
-                {children}
+                <div className={"container max-w-7xl mx-auto flex"}>
+                    <div className={"w-1/4 mr-10"}>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus animi autem, commodi
+                        consequatur eaque facere fuga fugit illo non pariatur possimus, quam quos rem, reprehenderit
+                        similique sit tenetur vero voluptatum? Animi aspernatur at consequatur consequuntur, cumque
+                        distinctio eligendi ex facilis fugit, hic maxime minus nam nihil nisi nulla quas quasi
+                        reprehenderit
+                        repudiandae sequi similique soluta ullam vel. Aperiam necessitatibus perferendis quaerat sit
+                        voluptatibus! Aliquam expedita impedit inventore perspiciatis praesentium qui similique sit.
+                        Amet
+                        error fugiat in ipsum iusto nulla rerum voluptate! Deleniti excepturi illo nihil qui quia unde
+                        velit. Ea earum error esse exercitationem ipsum nisi, optio possimus sapiente. Aperiam.
+                    </div>
+
+                    <div className={"w-3/4"}>
+                        <nav className="flex items-center text-gray-400 justify-between text xs mb-6">
+                            <ul className="flex uppercase font-semibold space-x-10 pb-3 border-b-4">
+                                <li><a className={"text-black border-b-4 pb-3 border-sky-500"} href="#">All Ideas
+                                    (87)</a></li>
+                                <li><a href="#">Considering</a></li>
+                                <li><a href="#">In Progress</a></li>
+                            </ul>
+
+                            <ul className="flex uppercase font-semibold space-x-10 pb-3 border-b-4">
+                                <li><a href="#">Implemented (10)</a></li>
+                                <li><a href="#">Closed (55)</a></li>
+                            </ul>
+                        </nav>
+
+                        <div className={""}>
+                            {children}
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     );
--- resources/js/Pages/Index.jsx
+++ resources/js/Pages/Index.jsx
@@ -6,34 +6,8 @@ export default function Index(props) {
         <AppLayout
             auth={props.auth}
         >
-            <main className="container mx-auto flex">
-                <div className={"w-1/4 mr-10"}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus animi autem, commodi
-                    consequatur eaque facere fuga fugit illo non pariatur possimus, quam quos rem, reprehenderit
-                    similique sit tenetur vero voluptatum? Animi aspernatur at consequatur consequuntur, cumque
-                    distinctio eligendi ex facilis fugit, hic maxime minus nam nihil nisi nulla quas quasi reprehenderit
-                    repudiandae sequi similique soluta ullam vel. Aperiam necessitatibus perferendis quaerat sit
-                    voluptatibus! Aliquam expedita impedit inventore perspiciatis praesentium qui similique sit. Amet
-                    error fugiat in ipsum iusto nulla rerum voluptate! Deleniti excepturi illo nihil qui quia unde
-                    velit. Ea earum error esse exercitationem ipsum nisi, optio possimus sapiente. Aperiam.
-                </div>
-                <div className={"w-3/4"}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad culpa enim harum inventore
-                    laborum provident, ullam veritatis. Ab atque earum excepturi ipsa iusto minima optio quaerat quos
-                    tenetur. Accusantium aperiam, debitis eius enim expedita nisi ratione sint tempora unde voluptates!
-                    Autem beatae consectetur, consequatur, doloribus, earum iste labore neque nisi numquam odit quis
-                    quod repellat tenetur voluptates voluptatibus. Aliquid asperiores assumenda consequuntur debitis
-                    deserunt, dolorem ducimus eos error exercitationem fuga hic illum minima nesciunt perspiciatis
-                    placeat praesentium quo reprehenderit sed tenetur voluptatem. Accusamus accusantium ad asperiores
-                    blanditiis, corporis dignissimos distinctio doloremque expedita ipsum mollitia nam obcaecati quidem
-                    sequi temporibus unde veniam voluptatem voluptatibus. Aliquam aliquid, asperiores autem consequuntur
-                    cumque deleniti dignissimos dolore dolorem doloribus dolorum, explicabo fugiat harum id labore,
-                    magni mollitia natus nihil officiis perspiciatis placeat quas quibusdam reiciendis saepe similique
-                    sit tenetur voluptates voluptatibus? Amet blanditiis ea esse eum labore mollitia omnis quis repellat
-                    sed, tenetur. Ab aut dolor doloremque odit quod reprehenderit suscipit, tempora. A aliquam,
-                    asperiores dignissimos ducimus eveniet fuga ipsam molestiae molestias natus necessitatibus non
-                    numquam odio pariatur qui similique tenetur vero. Ad alias aliquam beatae culpa doloribus ea eius
-                    eos illum incidunt ipsum, iusto laudantium nam obcaecati quibusdam quisquam quod ullam ut veniam!
-                </div>
-            </main>
+            <div className={"shadow rounded-md bg-white h-60"}>
+            </div>
         </AppLayout>
     )
 }
