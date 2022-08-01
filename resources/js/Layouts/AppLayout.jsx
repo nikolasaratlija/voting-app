4bd02a55c67e5b50646c7aad848e773b9030cd7d
Nikola Saratlija <saratlijanikola@gmail.com>
2022-08-01 23:56:05 +0200

    icon to input

--- resources/js/Layouts/AppLayout.jsx
+++ resources/js/Layouts/AppLayout.jsx
@@ -109,8 +109,19 @@ export default function AppLayout({auth, children}) {
                                 <option value="Category" disabled={true} selected>Other Filters</option>
                             </select>
 
-                            <input type="text" placeholder={"Find an idea"}
-                                   className={"w-2/4 shadow bg-white font-semibold rounded-xl border-none"}/>
+                            <div className={"w-2/4 relative"}>
+                                <div className={"flex absolute left-0 items-center h-full ml-2"}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6"
+                                         fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                                        <path strokeLinecap="round" strokeLinejoin="round"
+                                              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/>
+                                    </svg>
+                                </div>
+                                <input
+                                    type="text"
+                                    placeholder={"Find an idea"}
+                                    className={"pl-10 w-full shadow bg-white font-semibold rounded-xl border-none placeholder-gray-900"}/>
+                            </div>
 
                         </div>
 
