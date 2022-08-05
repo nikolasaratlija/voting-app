1255634cebd928f9ad70572e1ea8d7863f832194
Nikola Saratlija <saratlijanikola@gmail.com>
2022-08-05 16:18:44 +0200

    reduced test

--- tests/Feature/IdeaTest.php
+++ tests/Feature/IdeaTest.php
@@ -19,30 +19,16 @@ class IdeaTest extends TestCase
 
     public function test_index_shows_list_of_ideas()
     {
-        $categoryOne = Category::factory()->create();
-        $categoryTwo = Category::factory()->create();
-
-        $ideaOne = Idea::factory()->create([
-            'title' => 'First Idea',
-            'category_id' => $categoryOne->id,
-            'description' => 'First idea description'
-        ]);
-
-        $ideaTwo = Idea::factory()->create([
-            'title' => 'Second Idea',
-            'category_id' => $categoryTwo->id,
-            'description' => 'Second idea description'
-        ]);
+        $ideaOne = Idea::factory()->create();
+        $ideaTwo = Idea::factory()->create();
 
         $response = $this->get(route('ideas.index'));
 
         $response->assertSee($ideaOne->title);
         $response->assertSee($ideaOne->description);
-        $response->assertSee($categoryOne->name);
 
         $response->assertSee($ideaTwo->title);
         $response->assertSee($ideaTwo->description);
-        $response->assertSee($categoryTwo->name);
     }
 
     public function test_single_idea_shows_on_the_show_page()
